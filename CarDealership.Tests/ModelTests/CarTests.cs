78114079e6beee2db5b0cce2c680d16b3acb0cc5
RosarioRu <mariarruval@gmail.com>
2022-07-14 14:26:43 -0700

    Add boilerplate csproj file for tests directory and a ModelTests subdirectory containing file for tests.

---
+++ CarDealership.Tests/ModelTests/CarTests.cs
@@ -0,0 +1,21 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using ToDoList.Models;
+using System;
+
+namespace CarDealership.Tests
+{
+  [TestClass]
+  public class CarTests : 
+  {
+
+  //example test below:
+  //  [TestMethod]
+  //   public void ItemConstructor_CreatesInstanceOfItem_Item()
+  //   {
+  //     Item newItem = new Item("test");
+  //     Assert.AreEqual(typeof(Item), newItem.GetType());
+  //   }
+
+  }
+}
