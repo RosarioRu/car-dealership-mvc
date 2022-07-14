b36e20da4c4cf9f5e74dee8407a386f7c7fa8fa3
RosarioRu <mariarruval@gmail.com>
2022-07-14 16:36:10 -0700

    Write and pass first test that checks constructor creates an instance of Car.

--- CarDealership.Tests/ModelTests/CarTests.cs
+++ CarDealership.Tests/ModelTests/CarTests.cs
@@ -1,13 +1,19 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
-using ToDoList.Models;
+using CarDealership.Models;
 using System;
 
 namespace CarDealership.Tests
 {
   [TestClass]
-  public class CarTests : 
+  public class CarTests 
   {
+    [TestMethod] //first test
+    public void CarConstructor_CreatesAnInstanceOfCar_Car()
+    {
+      Car newCar = new Car("NissanAltima", 2500, 150000);
+      Assert.AreEqual(typeof(Car), newCar.GetType());
+    }
 
   //example test below:
   //  [TestMethod]
--- CarDealership/Models/Car.cs
+++ CarDealership/Models/Car.cs
@@ -5,7 +5,20 @@ using System;
 namespace CarDealership.Models
 {
   public class Car
-  {
+  { 
+    //properties
+    public string MakeModel {get;set;}
+    public int Price {get; set;}
+    public int Miles {get; set;}
+
+    //constructor
+    public Car(string makeModel, int price, int miles)
+    {
+      MakeModel = makeModel;
+      Price = price;
+      Miles = miles;
+    }
+
 
   }
 }
